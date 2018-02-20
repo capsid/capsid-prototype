51f275e2ca7efc93a645466c0598678f565c5b65
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-02-20 11:50:44 -0500

    ✨persisted queries (#9)

--- src/common/injectGlobals.js
+++ src/common/injectGlobals.js
@@ -8,10 +8,11 @@ let {
 } = process.env;
 
 export const apiRoot = REACT_APP_API_ROOT;
+
 export const egoApiRoot = REACT_APP_EGO_API_ROOT;
+export const egoAppId = REACT_APP_EGO_APP_ID;
 
 export const googleAppId = REACT_APP_GOOGLE_APP_ID;
-export const egoAppId = REACT_APP_EGO_APP_ID;
 
 export const defaultRedirectUri = REACT_APP_DEFAULT_REDIRECT_URI;
 export const allRedirectUris = REACT_APP_LOGIN_REDIRECT_NOT_REQUIRED.split(",")
