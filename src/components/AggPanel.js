8b2c674d06d65b42902f5e8c79d9f3f3c1fbfa3f
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-03-13 17:41:08 -0400

    :art: render aggs while data is loading (it's less janky)

--- src/components/AggPanel.js
+++ src/components/AggPanel.js
@@ -7,80 +7,72 @@ import {
   currentFieldValue
 } from "@arranger/components/dist/SQONView/utils";
 
-const AggPanel = ({ config, search, loading, sqon, updateSQON }) => (
+const aggDataFromSearch = ({ config, entity, field, search, type }) => {
+  const aggs = search[entity].aggs;
+  if (!aggs) return {};
+  const aggRoot = aggs[`${field}:global`]
+    ? aggs[`${field}:global`][`${field}:filtered`]
+    : aggs;
+  return aggRoot[type === "stats" ? `${field}:stats` : field];
+};
+
+const AggPanel = ({ config, search, sqon, updateSQON }) => (
   <div>
-    {!loading &&
-      _.flatten(
-        Object.keys(search)
-          .map(k => ({ entity: k, aggs: search[k].aggs }))
-          .filter(({ aggs }) => !!aggs)
-          .map(({ entity, aggs }) =>
-            config[entity].map(({ field, type, ...aggConfig }) => {
-              const aggRoot = aggs[`${field}:global`]
-                ? aggs[`${field}:global`][`${field}:filtered`]
-                : aggs;
-              return {
-                ...aggConfig,
-                field,
-                type,
-                entity,
-                data: aggRoot[type === "stats" ? `${field}:stats` : field]
-              };
-            })
-          )
-      ).map(
-        ({
-          entity,
-          field,
-          data,
-          type,
-          displayName,
-          namespacedField = `${entity}.${field}`,
-          key = field + JSON.stringify(data)
-        }) =>
-          type === "terms" ? (
-            <TermAgg
-              key={key}
-              field={namespacedField}
-              displayName={displayName}
-              buckets={data.buckets}
-              handleValueClick={({ generateNextSQON }) =>
-                updateSQON(generateNextSQON(sqon))
-              }
-              isActive={d =>
-                inCurrentSQON({
-                  value: d.value,
-                  dotField: d.field,
-                  currentSQON: sqon
-                })
-              }
-            />
-          ) : (
-            <RangeAgg
-              key={key}
-              field={namespacedField}
-              displayName={displayName}
-              stats={data}
-              value={{
-                min:
-                  currentFieldValue({
-                    sqon,
-                    dotField: namespacedField,
-                    op: ">="
-                  }) || data.min,
-                max:
-                  currentFieldValue({
-                    sqon,
-                    dotField: namespacedField,
-                    op: "<="
-                  }) || data.max
-              }}
-              handleChange={({ generateNextSQON }) =>
-                updateSQON(generateNextSQON(sqon))
-              }
-            />
-          )
-      )}
+    {_.flatten(
+      Object.keys(config).map(k => config[k].map(x => ({ ...x, entity: k })))
+    ).map(
+      ({
+        displayName,
+        entity,
+        field,
+        type,
+        namespacedField = `${entity}.${field}`,
+        data = aggDataFromSearch({ config, entity, field, search, type }) || {},
+        key = JSON.stringify({ namespacedField, data })
+      }) =>
+        type === "terms" ? (
+          <TermAgg
+            key={key}
+            field={namespacedField}
+            displayName={displayName}
+            buckets={data.buckets}
+            handleValueClick={({ generateNextSQON }) =>
+              updateSQON(generateNextSQON(sqon))
+            }
+            isActive={d =>
+              inCurrentSQON({
+                value: d.value,
+                dotField: d.field,
+                currentSQON: sqon
+              })
+            }
+          />
+        ) : (
+          <RangeAgg
+            key={key}
+            field={namespacedField}
+            displayName={displayName}
+            stats={data}
+            value={{
+              min:
+                currentFieldValue({
+                  sqon,
+                  dotField: namespacedField,
+                  op: ">="
+                }) || data.min,
+              max:
+                currentFieldValue({
+                  sqon,
+                  dotField: namespacedField,
+                  op: "<="
+                }) || data.max
+            }}
+            handleChange={({ generateNextSQON }) =>
+              updateSQON(generateNextSQON(sqon))
+            }
+          />
+        )
+    )}
   </div>
 );
 
